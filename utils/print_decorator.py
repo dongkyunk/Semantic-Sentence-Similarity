c9ac5127688aad0d915718c7e6695566e0f5ab57
DK <55306097+dongkyuk@users.noreply.github.com>
2020-11-05 15:47:53 +0900

    Update print_decorator.py

--- utils/print_decorator.py
+++ utils/print_decorator.py
@@ -4,6 +4,10 @@ from functools import wraps
 def print_if_complete(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        print(f'{func.__name__} complete')
-        return func(*args, **kwargs)
+        print(f'{func.__name__} starting')
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print('{} complete in {} seconds'.format(func.__name__, int(end-start)))
+        return result
     return wrapper
